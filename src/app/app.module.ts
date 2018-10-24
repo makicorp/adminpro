53f40a1f3a6f28fe5a3a94166c1c9492ff751802
Sistemas <makicorp@yahoo.com.mx>
2018-10-24 14:29:27 -0500

    Inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { APP_ROUTES } from './app.routes';
+import { RegisterComponent } from './login/register.component';
+import { PagesComponent } from './pages/pages.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumsComponent,
+    RegisterComponent,
+    PagesComponent
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTES
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
