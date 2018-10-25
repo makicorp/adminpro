84f3d42a4653a8d9a5f268c9825ed627ceba75f6
Sistemas <makicorp@yahoo.com.mx>
2018-10-25 14:57:25 -0500

    Modificaciones Modulos

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+
+
+import { NgModule } from '@angular/core';
+import { BreadcrumsComponent } from './breadcrums/breadcrums.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+@NgModule({
+    declarations: [
+        BreadcrumsComponent,
+        HeaderComponent,
+        SidebarComponent,
+        NopagefoundComponent
+    ],
+    exports: [
+        BreadcrumsComponent,
+        HeaderComponent,
+        SidebarComponent,
+        NopagefoundComponent
+    ],
+    providers: [],
+})
+export class SharedModule { }
