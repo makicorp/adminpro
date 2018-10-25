089e9ccd46917d2d345c7d8035c801f62f9b6663
Sistemas <makicorp@yahoo.com.mx>
2018-10-25 14:56:44 -0500

    Modificaciones Modulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,14 +8,14 @@ import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component
 import { RegisterComponent } from './login/register.component';
 
 const appRoutes: Routes = [
-    {path: '',
+  /*   {path: '',
     component: PagesComponent,
     children: [
         {path: 'dashboard', component: DashboardComponent},
         {path: 'progress', component: ProgressComponent},
         {path: 'grafica1', component: Grafica1Component},
         {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-    ]},
+    ]},*/
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
     {path: '**', component: NopagefoundComponent},
